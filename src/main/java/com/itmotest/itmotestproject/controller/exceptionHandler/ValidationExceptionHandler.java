cb75f41fc62c094187ab612c1cf9554ef8743af0
Dmitrii <dmitrii.zero@yandex.ru>
2025-01-17 14:37:55 +0300

    Добавляет сваггер

--- src/main/java/com/itmotest/itmotestproject/controller/exceptionHandler/ValidationExceptionHandler.java
+++ src/main/java/com/itmotest/itmotestproject/controller/exceptionHandler/ValidationExceptionHandler.java
@@ -1,19 +1,21 @@
 package com.itmotest.itmotestproject.controller.exceptionHandler;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@Hidden
+@RestControllerAdvice
 public class ValidationExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
